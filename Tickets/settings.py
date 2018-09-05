9b70b776891b6d79d0e5646260082a05ece55bac
scimmia <affe@live.cn>
2018-09-05 15:02:54 +0800

    数字千位按逗号分隔

--- Tickets/settings.py
+++ Tickets/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5q-@^^ylps#uddojtetfn*h!npradjnc-1i%#mqwdfb83zvmj$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+#
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+DEBUG = False
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
