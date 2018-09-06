46aa16c5fde7bf4458ce645657d7e17ce28bf43d
scimmia <affe@live.cn>
2018-09-06 15:26:52 +0800

    优化表格，为后二者增加查询功能

--- ticket/views.py
+++ ticket/views.py
@@ -377,6 +377,10 @@ def get_ticketlists(col):
 #显示各列表信息
 @login_required
 def ticket_list(request):
+    context = {
+        'gongyingshang': get_ticketlists('gongyingshang'),
+        'maipiaoren': get_ticketlists('maipiaoren'),
+    }
     #从根据不同的请求，来获取相应的数据,并跳转至相应页面
     if request.method == 'POST':
         log = LogTemp()
@@ -396,6 +400,7 @@ def ticket_list(request):
                         log.addunuse(-1*t.piaomianjiage * getPoolPercent().inpoolPer /100)
                         log.addstore(t.gourujiage)
                 log.save()
+                context['message'] = u'入库成功'
             pass
         elif 'all_topool' in request.POST.keys():
             print('all_topool')
@@ -413,65 +418,22 @@ def ticket_list(request):
                         log.addstore(-1*t.gourujiage)
                         log.addunuse(t.piaomianjiage * getPoolPercent().inpoolPer /100)
                 log.save()
+                context['message'] = u'入池成功'
             pass
     # 将原先的data更名为raw_data
     raw_data = Ticket.objects.all().order_by('-goumairiqi')
     print(raw_data)
     list_template = 'ticket/ticket_list.html'
 
-    #通过GET方法从提交的URL来获取相应参数
-    #建立一个空的参数的字典
-    kwargs = {}
-    #建立一个空的查询语句
-    query = ''
-    #提交过来的GET值是一个迭代的键值对
-    for key in request.GET.keys():
-        value = request.GET[(key)]
-        print(key,value)
-        #刨去其中的token和page选项
-        if key != 'csrfmiddlewaretoken' and key != 'page' and (len(value)>0):
-            #由于线路和设备的外键均与node表格有关，当查询线路中的用户名称或设备信息中的使用部门时，可以直接通过以下方式跨表进行查找
-            if key == 'node':
-                kwargs['node__node_name__contains'] = value
-                #该query用于页面分页跳转时，能附带现有的搜索条件
-                query += '&' + key + '=' + value
-            #其余的选项均通过key来辨别
-            else:
-                kwargs[key] = value
-                #该query用于页面分页跳转时，能附带现有的搜索条件
-                query += '&' + key + '=' + value
-    #通过元始数据进行过滤，过滤条件为健对值的字典
-    data = raw_data.filter(**kwargs)
-    #如果没有从GET提交中获取信息，那么data则为元始数据
-
-    #将分页的信息传递到展示页面中去
-    data_list, page_range, count, page_nums = pagination(request, data,30)
-    #建立context字典，将值传递到相应页面
-    context = {
-        'data': data_list,
-        'gongyingshang': get_ticketlists('gongyingshang'),
-        'maipiaoren': get_ticketlists('maipiaoren'),
-        'query': query,
-        'page_range': page_range,
-        'count': count,
-        'page_nums': page_nums,
-        'filter': filter,
-    }
-    print(context)
-    #跳转到相应页面，并将值传递过去
-    return render(request,list_template,context)
+    return getPagedPage(request,raw_data,list_template,context)
 
 @login_required
 def tickets_needfix(request):
-    raw_data = Ticket.objects.filter(pay_status=1,payorder=None,gourujiage=0).order_by('-goumairiqi')
-    print(raw_data)
-    list_template = 'ticket/tickets_needfix.html'
     context = {
-        'data': raw_data,
+        'gongyingshang': get_ticketlists('gongyingshang'),
     }
-    print(context)
-    #跳转到相应页面，并将值传递过去
-    return render(request,list_template,context)
+    raw_data = Ticket.objects.filter(pay_status=1,payorder=None,gourujiage=0).order_by('-goumairiqi')
+    return getPagedPage(request, raw_data,'ticket/tickets_needfix.html',context)
 
 def ticket_needselect(request,index):
     if request.method == 'GET':
@@ -1492,6 +1454,10 @@ def pool_loan_repay(request):
     return redirect('pool_loans')
 
 def pool_tickets(request):
+    context = {
+        'gongyingshang': get_ticketlists('gongyingshang'),
+        'maipiaoren': get_ticketlists('maipiaoren'),
+    }
     if request.method == 'POST':
         if 'ids' in request.POST.keys():
             ids = request.POST['ids']
@@ -1508,15 +1474,11 @@ def pool_tickets(request):
                         log.addpro(0-t.gourujiage)
                         log.addunuse(0-t.gourujiage)
             log.save()
+            context['message'] = u'还款成功'
             pass
-
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     raw_data = Ticket.objects.filter(Q(gouruzijinchi=True)&Q(payedzijinchi=False)&Q(daoqiriqi__lte=today)).order_by('-goumairiqi')
-    context = {
-        'data': raw_data,
-    }
-    #与res_add.html用同一个页面，只是edit会在res_add页面做数据填充
-    return render(request, 'ticket/pool_tickets.html', context)
+    return getPagedPage(request,raw_data, 'ticket/pool_tickets.html', context)
 
 
 def pool_pro(request):
@@ -1748,6 +1710,28 @@ def counter(request):
     return render(request, 'ticket/tool_counter.html')
 def tiexian(request):
     return render(request, 'ticket/tool_tiexian.html')
+def getQuery(request):
+    kwargs = {}
+    query = ''
+    for key in request.GET.keys():
+        value = request.GET[(key)]
+        #刨去其中的token和page选项
+        if key != 'csrfmiddlewaretoken' and key != 'page' and (len(value)>0):
+            kwargs[key] = value
+            query += '&' + key + '=' + value
+    return kwargs,query
+
+def getPagedPage(request,raw_data,list_template,context = {}):
+    kwargs,query = getQuery(request)
+    data = raw_data.filter(**kwargs)
+    data_list, page_range, count, page_nums = pagination(request, data,30)
+    context['data'] = data_list
+    context['query'] = query
+    context['page_range'] = page_range
+    context['count'] = count
+    context['page_nums'] = page_nums
+    return render(request,list_template,context)
+
 #分页函数
 def pagination(request, queryset, display_amount=10, after_range_num = 5,before_range_num = 4):
     #按参数分页
